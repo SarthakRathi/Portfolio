ca80fb8b9072dd68ccaabcd8edf4bea734729f71
SarthakRathi <sarthakrathi89@gmail.com>
2025-04-12 00:23:01 +0530

    bugs

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'Portfolio', // Add this line with your GitHub repo name
 })
